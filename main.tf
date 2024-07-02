092cd6c27555b3151504ef857be0483227d3fa43
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2024-07-03 00:27:16 +0900

    인터넷 게이트웨이 등 외부 통신 가능하도록 변경

--- main.tf
+++ main.tf
@@ -12,10 +12,29 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = var.availability_zone
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_security_group" "allow_ssh" {
@@ -46,16 +65,13 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "vault" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.main.id
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   key_name               = aws_key_pair.generated_key.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "Vault-Server"
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 30"
   }
 
   provisioner "remote-exec" {
@@ -84,16 +100,13 @@ resource "aws_instance" "ssh_test" {
   count                  = 2
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.main.id
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   key_name               = aws_key_pair.generated_key.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "SSH-Test-Instance-${count.index + 1}"
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 30"
   }
 
   provisioner "remote-exec" {
